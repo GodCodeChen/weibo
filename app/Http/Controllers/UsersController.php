f06ea815ad3dc37460f77088cab5a3d7b4a8ed5a
vagrant <cq19971103@gmail.com>
2019-09-24 08:27:19 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,19 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
+    //    创建用户页面
     public function create()
     {
         return view('users.create');
     }
 
+    //    个人中心
     public function show(User $user)
     {
         return view('users.show',compact('user'));
     }
 
+    //    用户注册
     public function store(Request $request)
     {
         $this->validate($request,[
@@ -31,6 +35,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
+        Auth::login($user);
         session()->flash('success',"欢迎，您将在这里开启一段新旅程~");
 
         return redirect()->route('users.show',[$user]);
